be8f3b042ae45c41a63bca34c5d072c90f1ca529
Tash Hossain <tashyhossain@gmail.com>
2021-06-19 18:00:52 -0400

    Added color picker and soft brush

--- script.js
+++ script.js
@@ -14,31 +14,55 @@ function createGrid(size = 16) {
     }
   }
 
+  let brush = document.querySelector('#soft');
+  let color = document.querySelector('#color-picker');
   let cells = document.querySelectorAll('.grid-cell');
-  cells.forEach(cell => cell.addEventListener('mouseover', () => {
-    cell.classList.add('fill-cell');
-  }));
-  return cells;
-}
+  cells.forEach(cell => { 
+    let alpha = 0;
+    cell.addEventListener('mouseover', () => {
+      cell.style.backgroundColor = color.value;
+      if (brush.selected == true) {
+        alpha += 0.2;
+        cell.style.opacity = alpha;
+      } else {
+        cell.style.opacity = 1;
+      }
+    });
+  });
 
-createGrid();
+}
 
 function clearGrid() {
   let cells = document.querySelectorAll('.grid-cell');
-  cells.forEach(cell => cell.classList.remove('fill-cell'));
+  cells.forEach(cell => { 
+    cell.style.backgroundColor = '#202328';
+    cell.style.opacity = 1;
+  });
 }
 
 const clearBtn = document.querySelector('#clear-grid');
 clearBtn.addEventListener('click', clearGrid);
 
-const newGrid = document.querySelector('#new-grid');
-newGrid.addEventListener('click', () => {
+let slider = document.querySelector('#slider');
+let output = document.querySelector('#slider-info');
+
+slider.addEventListener('input', () => {
+  let newSize = slider.value;
+  output.textContent = `${newSize} x ${newSize}`;
   wipe();
-  let newSize = prompt('Set new size (max 100)', 16);
   createGrid(newSize);
 });
+
+// const newGrid = document.querySelector('#new-grid');
+// newGrid.addEventListener('click', () => {
+//   wipe();
+//   let newSize = prompt('Set new size (max 100)', 16);
+//   createGrid(newSize);
+// });
 
 function wipe() {
   let cells = document.querySelectorAll('.grid-cell');
   cells.forEach(cell => grid.removeChild(cell));
 }
+
+createGrid();
