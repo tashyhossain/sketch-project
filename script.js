2d711c76aada76a3bad7e6f6925e7fd13aaa09a8
Tash Hossain <tashyhossain@gmail.com>
2021-06-19 05:03:23 -0400

    Interface added

--- script.js
+++ script.js
@@ -1,54 +1,44 @@
-let container = document.querySelector('#container');
+const grid = document.querySelector('#container');
 
 function createGrid(size = 16) {
-  let cellWidth = 18;
-  let cellHeight = 18;
-
-  container.style.width = `${size * (cellWidth + 2) }px`;
-  container.style.height = `${size * (cellHeight + 2)}px`;
-
+  let cellWidth = 320/size - 2;
+  let cellHeight = 320/size - 2;
+  
   for (let y = 0; y < size; y++) {
     for (let x = 0; x < size; x++) {
-      let div = document.createElement('div');
-      div.style.width = `${cellWidth}px`;
-      div.style.height = `${cellHeight}px`;
-      div.classList.add('grid-cell');
-      container.appendChild(div);
+      let cell = document.createElement('div');
+      cell.style.width = `${cellWidth}px`;
+      cell.style.height = `${cellHeight}px`;
+      cell.classList.add('grid-cell');
+      grid.appendChild(cell);
     }
   }
 
-  let divs = document.querySelectorAll('.grid-cell');
-  divs.forEach(div => div.addEventListener('mouseover', () => {
-    div.classList.add('fill-cell');
+  let cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => cell.addEventListener('mouseover', () => {
+    cell.classList.add('fill-cell');
   }));
-  return divs;
+  return cells;
 }
 
 createGrid();
 
-let controls = document.querySelector('#controls');
-let clearBtn = document.createElement('button');
-clearBtn.textContent = 'Clear Grid';
-controls.appendChild(clearBtn);
-
-clearBtn.addEventListener('click', clearGrid);
-
 function clearGrid() {
-  let divs = document.querySelectorAll('.grid-cell');
-  divs.forEach(div => div.classList.remove('fill-cell'));
+  let cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => cell.classList.remove('fill-cell'));
 }
 
-let newBtn = document.createElement('button');
-newBtn.textContent = 'New Grid';
-controls.appendChild(newBtn);
+const clearBtn = document.querySelector('#clear-grid');
+clearBtn.addEventListener('click', clearGrid);
 
-newBtn.addEventListener('click', () => {
+const newGrid = document.querySelector('#new-grid');
+newGrid.addEventListener('click', () => {
   wipe();
-  createGrid(prompt('Set new grid size (max 100)', 16));
+  let newSize = prompt('Set new size (max 100)', 16);
+  createGrid(newSize);
 });
 
 function wipe() {
-  let divs = document.querySelectorAll('.grid-cell');
-  divs.forEach(div => container.removeChild(div));
+  let cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => grid.removeChild(cell));
 }
-
